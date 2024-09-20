ad585459cfb530ecd7649225a9cadf3aa9cb1c41
Alto0327 <aj.silvaf2003@gmail.com>
2024-09-20 14:52:41 -0400

    CartSlice jsx modifications

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,24 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+      const item = action.payload;
+      const existingItem = state.items.find((i) => i.id === item.id);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ ...item, quantity: 1 });
+      }
     
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { itemId, quantity } = action.payload;
+      const existingItem = state.items.find((i) => i.id === itemId);
+      if (existingItem) {
+        existingItem.quantity = quantity;
+      }
     },
   },
 });
