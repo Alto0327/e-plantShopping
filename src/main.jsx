6c6241b89c7cd14ce0bc3045fb6243a712cc5a6b
Alto0327 <aj.silvaf2003@gmail.com>
2024-09-20 15:25:50 -0400

    Refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <React.StrictMode>
     <Provider store={store}>
     <App />
     </Provider>
-  </HashRouter>
+  </React.StrictMode>,
 )
